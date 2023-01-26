47c74a2c11153fc0be36943a68d30490f65289c2
goncaloramos <goncalo91ramos@gmail.com>
2023-01-26 22:53:21 +0000

    change app repo

--- infra/main.tf
+++ infra/main.tf
@@ -117,8 +117,8 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
-  branch             = "master"
+  repo_url           = "https://github.com/goncaloramospt/knox-presentation/"
+  branch             = "main"
   use_manual_integration = true
   use_mercurial      = false
 }
